b395f775470dd3c18ba5c1b6d6a0601e11d45370
ZackHiRo <109440332+ZackHiRo@users.noreply.github.com>
2023-09-08 03:24:49 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+/**
+ * Description: A C program that prints with put function
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
