c39d1cf26892496f645218261cd8b1fce4e6b82c
Zakaria <aoun.zakaria@gmail.com>
2023-10-22 07:14:19 -0700

    0x08-recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse, recursively.
- * @s: The string to be printed in reverse.
+ * _puts_recursion - Prints a string, followed by a new line, recursively.
+ * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-    if (*s == '\0') // Base case: if the current character is null terminator
-        return;
-    
-    _print_rev_recursion(s + 1); // Recur for the next character in the string
-    _putchar(*s); // Print the current character
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 
