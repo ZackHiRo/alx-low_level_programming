f07e61f3014e5433e277e956ad3c96293d4640a9
Zakaria <aoun.zakaria@gmail.com>
2023-10-24 01:36:56 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,13 +12,13 @@ listint_t *temp;
 int data = 0;
 
 if (head == NULL || *head == NULL)
-return 0;
+return (0);
 
 temp = *head;
 data = temp->n;
 *head = (*head)->next;
 free(temp);
 
-return data;
+return (data);
 }
 
