e98c538a03d9756b04f2b1d944db62a145887106
Zakaria <aoun.zakaria@gmail.com>
2023-10-24 16:18:00 -0700

    Linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-    
+
 while (h != NULL)
 {
 printf("[%u] %s\n", h->len, (h->str != NULL ? h->str : "(nil)"));
 h = h->next;
 count++;
 }
-    
+
 return (count);
 }
 
