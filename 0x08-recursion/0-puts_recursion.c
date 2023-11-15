a8a96447e299658c8331f15ccddde9853364347b
Zakaria <aoun.zakaria@gmail.com>
2023-11-15 13:49:39 -0800

    Alx Recursion Project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s == '\0') // Base case: if the current character is null terminator
+if (*s == '\0') /* Base case: if the current character is null terminator */
 {
-_putchar('\n'); // Print a new line
+_putchar('\n'); /* Print a new line */
 return;
 }
-_putchar(*s); // Print the current character
-_puts_recursion(s + 1); // Recur for the next character in the string
+_putchar(*s); /* Print the current character */
+_puts_recursion(s + 1); /* Recur for the next character in the string */
 }
 
