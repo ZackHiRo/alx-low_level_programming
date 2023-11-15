77f975a5f1ef4a05f50218498ff1da300fcb8677
Zakaria <aoun.zakaria@gmail.com>
2023-11-15 14:12:32 -0800

    Alx Recursion Project

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _pow_recursion - Returns the value of x raised to the power of y.
+* @x: The base.
+* @y: The exponent.
+* Return: x^y, -1 if y is negative.
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0) /* Base case: if y is negative, return -1 (error)*/
+return (-1);
+if (y == 0) /* Base case: any number raised to the power of 0 is 1*/
+return (1);
+
+return x * _pow_recursion(x, y - 1);
+/* Recur with y-1 and multiply the result with x */
+}
+
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - Returns the natural square root of a number.
+* @n: The number to find the square root of.
+* Return: Square root of n, -1 if n does not have a natural square root.
+*/
+int _sqrt_recursion(int n)
+{
+return find_sqrt(n, 1); // Start the search for square root from 1
+}
+
+/**
+* find_sqrt - Helper function to find the square root recursively.
+* @n: The number to find the square root of.
+* @i: The current guess for the square root.
+* Return: Square root of n, -1 if n does not have a natural square root.
+*/
+int find_sqrt(int n, int i)
+{
+if (i * i > n) /* If i*i exceeds n, n doesn't have a natural square root */
+return (-1);
+if (i * i == n) /* If i*i equals n, i is the square root of n*/
+return (i);
+
+return (find_sqrt(n, i + 1)); /* Recur with the next guess for the square root */
+}
+
