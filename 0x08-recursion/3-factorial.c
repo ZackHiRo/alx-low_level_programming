84482cff299ab84a1495090f4f9588e0d4a138b9
Zakaria <aoun.zakaria@gmail.com>
2023-11-15 14:00:47 -0800

    Alx Recursion Project

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,12 @@
  */
 int factorial(int n)
 {
-    if (n < 0) /* Base case: if n is negative, return -1 (error) */
-        return -1;
-    if (n == 0) /* Base case: factorial of 0 is 1 */
-        return 1;
+if (n < 0) /* Base case: if n is negative, return -1 (error) */
+return (-1);
+if (n == 0) /* Base case: factorial of 0 is 1 */
+return (1);
     
-    return (n * factorial(n - 1));
+return (n * factorial(n - 1));
 /* Recur with n-1 and multiply the result with n */
 }
 
