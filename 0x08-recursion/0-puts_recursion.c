556d4b36ffc8d182288b73c39dd9dc0d2b1abc06
Zakaria <aoun.zakaria@gmail.com>
2023-11-15 13:47:15 -0800

    Alx Recursion Project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * puts_recursion - Prints a string using recursion.
- *
- * This function prints each character of the string `s`
- * followed by a newline character using recursion.
- *
- * @s: The input string.
- */
-void puts_recursion(char *s)
+* _puts_recursion - Prints a string, followed by a new line, recursively.
+* @s: The string to be printed.
+*/
+void _puts_recursion(char *s)
 {
-*s ? (putchar(*s), puts_recursion(s + 1)) : putchar('\n');
+if (*s == '\0') // Base case: if the current character is null terminator
+{
+_putchar('\n'); // Print a new line
+return;
+}
+_putchar(*s); // Print the current character
+_puts_recursion(s + 1); // Recur for the next character in the string
 }
 
