5627e7000f0677649ea0193a19dd2147fc9ffe21
Zakaria <aoun.zakaria@gmail.com>
2023-11-15 14:02:03 -0800

    Alx Recursion Project

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,7 +11,7 @@ if (n < 0) /* Base case: if n is negative, return -1 (error) */
 return (-1);
 if (n == 0) /* Base case: factorial of 0 is 1 */
 return (1);
-    
+
 return (n * factorial(n - 1));
 /* Recur with n-1 and multiply the result with n */
 }
