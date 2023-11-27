8640fe6d497026c054121cd0956c3a0fad3a8a3f
Zakaria <aoun.zakaria@gmail.com>
2023-11-27 08:07:51 -0800

    ALX Preprocessor Project

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the file it was compiled from
+ *              and returns 0.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
+/* Print the name of the file using the __FILE__ macro */
 printf("%s\n", __FILE__);
+
+/* Return 0 to indicate successful execution */
 return (0);
 }
 
