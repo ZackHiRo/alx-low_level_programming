90701a7bc26a8649e01a0c356fb46110eec70686
Zakaria <aoun.zakaria@gmail.com>
2023-11-27 08:16:02 -0800

    ALX Preprocessor Project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a structure representing a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+/* Function prototypes */
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
+
